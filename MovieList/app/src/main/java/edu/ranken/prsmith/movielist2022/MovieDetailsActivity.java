ef61caac6a4ddb92f4f7d7bcaefc3349a4da0b7d
Paul Smith <prsmith@ranken.edu>
2022-03-03 10:26:35 -0600

    fab and scrollview

--- MovieList/app/src/main/java/edu/ranken/prsmith/movielist2022/MovieDetailsActivity.java
+++ MovieList/app/src/main/java/edu/ranken/prsmith/movielist2022/MovieDetailsActivity.java
@@ -5,9 +5,11 @@ import androidx.lifecycle.ViewModelProvider;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.android.material.snackbar.Snackbar;
+import com.squareup.picasso.Picasso;
 
 import edu.ranken.prsmith.movielist2022.ui.MovieDetailsViewModel;
 
@@ -20,25 +22,37 @@ public class MovieDetailsActivity extends BaseActivity {
     // state
     private String movieId;
     private MovieDetailsViewModel model;
+    private Picasso picasso;
 
     // views
     private TextView movieErrorText;
     private TextView movieTitleText;
     private TextView movieDescriptionText;
+    private ImageView movieBanner;
+    private ImageView[] movieScreenshots;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_movie_details);
+        setContentView(R.layout.activity_movie_details_scroll);
 
         // find views
         movieErrorText = findViewById(R.id.movieErrorText);
         movieTitleText = findViewById(R.id.movieTitleText);
         movieDescriptionText = findViewById(R.id.movieDescriptionText);
+        movieBanner = findViewById(R.id.movieBannerImage);
+        movieScreenshots = new ImageView[] {
+            findViewById(R.id.movieScreenshot1),
+            findViewById(R.id.movieScreenshot2),
+            findViewById(R.id.movieScreenshot3)
+        };
 
         // get intent
         Intent intent = getIntent();
         movieId = intent.getStringExtra(EXTRA_MOVIE_ID);
+
+        // get picasso
+        picasso = Picasso.get();
 
         // bind model
         model = new ViewModelProvider(this).get(MovieDetailsViewModel.class);
@@ -57,6 +71,19 @@ public class MovieDetailsActivity extends BaseActivity {
                     movieDescriptionText.setText(R.string.noDescription);
                 } else {
                     movieDescriptionText.setText(movie.longDescription);
+                }
+                if (movie.bannerUrl == null) {
+                    movieBanner.setImageResource(R.drawable.ic_broken_image);
+                } else {
+                    movieBanner.setImageResource(R.drawable.ic_downloading);
+                    picasso
+                        .load(movie.bannerUrl)
+                        .noPlaceholder()
+                        //.placeholder(R.drawable.ic_downloading)
+                        .error(R.drawable.ic_error)
+                        .resize(800, 200)
+                        .centerInside()
+                        .into(movieBanner);
                 }
             }
         });
--- MovieList/app/src/main/java/edu/ranken/prsmith/movielist2022/data/Movie.java
+++ MovieList/app/src/main/java/edu/ranken/prsmith/movielist2022/data/Movie.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 
 import com.google.firebase.firestore.DocumentId;
 
+import java.util.List;
 import java.util.Map;
 
 public class Movie {
@@ -12,9 +13,12 @@ public class Movie {
     public String name;
     public String director;
     public Integer releaseYear;
-    public String image;
     public Map<String, Boolean> genre;
     public String longDescription;
+
+    public String image;  // thumbnail
+    public String bannerUrl;
+    public List<String> screenshotUrls;
 
     public Movie() {}
 
