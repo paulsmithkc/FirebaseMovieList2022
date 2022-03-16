e60ff51226f07518340db28d0afcb7c5f20f977f
Paul Smith <prsmith@ranken.edu>
2022-03-16 16:47:12 -0500

    removed bottom nav

--- TabbedNavigation2/app/src/main/java/edu/ranken/prsmith/bottomnavigation/MainActivity.java
+++ TabbedNavigation2/app/src/main/java/edu/ranken/prsmith/bottomnavigation/MainActivity.java
@@ -16,11 +16,8 @@ import com.google.android.material.tabs.TabLayout;
 import edu.ranken.prsmith.bottomnavigation.ui.MainPageAdapter;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String LOG_TAG = MainActivity.class.getSimpleName();
-
     private TabLayout tabLayout;
     private ViewPager2 pager;
-    private BottomNavigationView bottomNav;
     private MainPageAdapter adapter;
 
     @Override
@@ -31,7 +28,6 @@ public class MainActivity extends AppCompatActivity {
         // find views
         tabLayout = findViewById(R.id.tabLayout);
         pager = findViewById(R.id.pager);
-        bottomNav = findViewById(R.id.bottomNav);
 
         // create adapter
         adapter = new MainPageAdapter(this);
@@ -46,11 +42,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onPageSelected(int position) {
                 super.onPageSelected(position);
-                Log.i(LOG_TAG, "Selected page: " + position);
-
-                Menu menu = bottomNav.getMenu();
-                menu.getItem(position).setChecked(true);
-
                 TabLayout.Tab tab = tabLayout.getTabAt(position);
                 tabLayout.selectTab(tab);
             }
@@ -69,21 +60,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onTabUnselected(TabLayout.Tab tab) {
                 // do nothing
-            }
-        });
-        bottomNav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int itemId = item.getItemId();
-                if (item.getItemId() == R.id.actionMoviesList) {
-                    pager.setCurrentItem(MainPageAdapter.POS_MOVIE_LIST);
-                    return true;
-                } else if (itemId == R.id.actionUsersList) {
-                    pager.setCurrentItem(MainPageAdapter.POS_USER_LIST);
-                    return true;
-                } else {
-                    return false;
-                }
             }
         });
     }
