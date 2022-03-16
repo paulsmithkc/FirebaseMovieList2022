e64d5a86d06bd0085b379114780696b4140cfd1d
Paul Smith <prsmith@ranken.edu>
2022-03-16 13:17:53 -0500

    implement TabbedNavigation project

--- TabbedNavigation/app/src/main/java/edu/ranken/prsmith/tabbednavigation/MainActivity.java
+++ TabbedNavigation/app/src/main/java/edu/ranken/prsmith/tabbednavigation/MainActivity.java
@@ -1,14 +1,38 @@
 package edu.ranken.prsmith.tabbednavigation;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+import edu.ranken.prsmith.tabbednavigation.ui.ExamplePageAdapter;
+
 public class MainActivity extends AppCompatActivity {
+    private TabLayout tabLayout;
+    private ViewPager2 pager;
+    private ExamplePageAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // find views
+        tabLayout = findViewById(R.id.tabLayout);
+        pager = findViewById(R.id.pager);
+
+        // create adapter
+        adapter = new ExamplePageAdapter(this);
+        pager.setAdapter(adapter);
+
+        // create mediator
+        TabLayoutMediator mediator = new TabLayoutMediator(
+            tabLayout, pager,
+            (tab, position) -> tab.setText("TAB " + position)
+        );
+        mediator.attach();
     }
 }
---
+++ TabbedNavigation/app/src/main/java/edu/ranken/prsmith/tabbednavigation/ui/ExamplePageFragment.java
@@ -0,0 +1,34 @@
+package edu.ranken.prsmith.tabbednavigation.ui;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import java.text.NumberFormat;
+
+import edu.ranken.prsmith.tabbednavigation.R;
+
+public class ExamplePageFragment extends Fragment {
+    public ExamplePageFragment() {
+        super(R.layout.fragment_page);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        // get args
+        Bundle args = requireArguments();
+        int position = args.getInt("position");
+
+        // find views
+        TextView textView = view.findViewById(R.id.textView);
+
+        // populate views
+        NumberFormat numberFormat = NumberFormat.getIntegerInstance();
+        textView.setText(numberFormat.format(position));
+    }
+}
+
