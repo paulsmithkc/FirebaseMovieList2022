a3582fc70fa93013441c2ce530a705c26b5f54bc
Paul Smith <prsmith@ranken.edu>
2022-04-19 08:42:50 -0500

    adding missing activities

---
+++ MovieList/app/src/main/java/edu/ranken/prsmith/movielist2022/ComposeReviewActivity.java
@@ -0,0 +1,14 @@
+package edu.ranken.prsmith.movielist2022;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ComposeReviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.review_compose);
+    }
+}
---
+++ MovieList/app/src/main/java/edu/ranken/prsmith/movielist2022/EbayBrowseActivity.java
@@ -0,0 +1,14 @@
+package edu.ranken.prsmith.movielist2022;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EbayBrowseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ebay_browse);
+    }
+}
